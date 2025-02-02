b90d4681c249acc8f24dfce9bf4424ad4685a531
sien <sptw88@gmail.com>
2025-02-02 15:16:34 +0900

    Changes

--- model.py
+++ model.py
@@ -1,33 +1,34 @@
 import torch
-from sympy.crypto import padded_key
-from transformers import GPT2LMHeadModel
-from transformers import GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from transformers import GPT2Config
 import transformers
 import torch.nn as nn
-from transformers.models.gpt2.modeling_gpt2 import GPT2MLP
 import math
 import torch.nn.functional as F
 
 
 class GPT2(nn.Module):
     def __init__(self, config):
-        super(GPT2, self).__init__(config)
+        super(GPT2, self).__init__()
         self.embed_dim = config.hidden_size
-        self.wte = nn.Embedding(config.vocab_size, self.embed_dim)
-        self.wpe = nn.Embedding(config.max_position_embedding, self.embed_dim)
-        self.drop = nn.Dropout(config.embd_pdrop)
-        self.h = nn.ModuleList([GPT2Block(config) for _ in range(config.num_hidden_layers)])
-        self.ln_f = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_epsilon)
+
+        self.transformer = nn.ModuleDict(dict(
+            wte=nn.Embedding(config.vocab_size, self.embed_dim),
+            wpe=nn.Embedding(config.max_position_embedding, self.embed_dim),
+            drop=nn.Dropout(config.embd_pdrop),
+            h=nn.ModuleList([GPT2Block(config) for _ in range(config.n_layer)]),
+            ln_f=nn.LayerNorm(self.embed_dim, eps=config.layer_norm_epsilon)
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
     def forward(self, input_ids, labels=None, pad_mask=None):
-        input_embeds = self.wte(input_ids)
-        position_ids = self.wpe(
-            torch.arange(0, input_ids.size(1), input_ids.size(1), device=input_ids.device, dtype=torch.long))
-        position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
+        input_embeds = self.transformer.wte(input_ids)
+        position_ids = self.transformer.wpe(
+            torch.arange(0, input_ids.size(1), device=input_ids.device)).unsqueeze(0)
         hidden_states = input_embeds + position_ids
-        hidden_states = self.drop(hidden_states)
-        hidden_states = self.h(hidden_states, pad_mask)
+        hidden_states = self.transformer.drop(hidden_states)
+        for block in self.transformer.h:
+            hidden_states = block(hidden_states,pad_mask=pad_mask)
         lm_logits = self.lm_head(hidden_states)
         loss = None
         if labels is not None:
@@ -35,9 +36,25 @@ class GPT2(nn.Module):
             shift_logits = lm_logits[..., :-1, :].contiguous()
             shift_labels = labels[..., 1:].contiguous()
             loss = F.cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-            return loss
-        return lm_logits
 
+        return lm_logits, loss
+
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens, pad_mask=None, temperature=0.8, do_sample=False, top_k=None):
+        for _ in range(max_new_tokens):
+            idx_cond = idx if idx.size(1) <= 1024 else idx[:, -1024:]
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :] / temperature
+            if top_k is not None:
+                v, _ = torch.topk(logits, top_k)
+                logits[logits < v[:, [-1]]] = -float('Inf')
+            probs = F.softmax(logits, dim=-1)
+            if do_sample:
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                _, idx_next = torch.topk(probs, k=1, dim=-1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
 
 
 class GPT2Block(nn.Module):
@@ -52,7 +69,7 @@ class GPT2Block(nn.Module):
     def forward(self, hidden_states, pad_mask=None):
         residual = hidden_states
         hidden_states = self.ln_1(hidden_states)
-        attn_out = self.attn(hidden_states,pad_mask)
+        attn_out = self.attn(hidden_states, pad_mask)
         hidden_states = attn_out + residual
         residual = hidden_states
         hidden_states = self.ln_2(hidden_states)
@@ -61,23 +78,20 @@ class GPT2Block(nn.Module):
         return hidden_states
 
 
-class NewGELUActivation(nn.Module):
-    """
-    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT). Also see
-    the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
-    """
-
-    def forward(self, input):
-        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
+class NewGELU(nn.Module):
+    def forward(self, x):
+        return 0.5 * x * (1.0 + torch.tanh(
+            math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))
+        ))
 
 
 class GPT2MLP(nn.Module):
     def __init__(self, intermediate_size, config):
         super().__init__()
         embed_dim = config.hidden_size
-        self.c_fc = nn.Conv1d(intermediate_size, embed_dim)
-        self.c_proj = nn.Conv1d(embed_dim, intermediate_size)
-        self.act = NewGELUActivation()
+        self.c_fc = nn.Linear(embed_dim,intermediate_size)
+        self.c_proj = nn.Linear(intermediate_size,embed_dim)
+        self.act = NewGELU()
         self.dropout = nn.Dropout(config.resid_pdrop)
 
     def forward(self, hidden_states) -> torch.FloatTensor:
@@ -91,7 +105,7 @@ class GPT2MLP(nn.Module):
 class GPT2Attention(nn.Module):
     def __init__(self, config):
         super(GPT2Attention, self).__init__()
-        max_positions = config.max_position_embeddings
+        max_positions = config.n_positions
         self.register_buffer(
             "bias",
             torch.tril(torch.ones((max_positions, max_positions), dtype=torch.bool)).view(
@@ -99,12 +113,12 @@ class GPT2Attention(nn.Module):
             ),
             persistent=False,
         )
-        self.embed_dim = config.hidden_size
-        self.num_heads = config.num_attention_heads
+        self.embed_dim = config.n_embd
+        self.num_heads = config.n_head
         self.head_dim = self.embed_dim // self.num_heads
         self.split_size = self.embed_dim
-        self.c_attn = nn.Conv1d(3 * self.embed_dim, self.embed_dim)
-        self.c_proj = nn.Conv1d(self.embed_dim, self.embed_dim)
+        self.c_attn = nn.Linear(self.embed_dim,3 * self.embed_dim)
+        self.c_proj = nn.Linear(self.embed_dim, self.embed_dim)
         self.attn_dropout = nn.Dropout(config.attn_pdrop)
         self.resid_dropout = nn.Dropout(config.resid_pdrop)
 
@@ -114,20 +128,86 @@ class GPT2Attention(nn.Module):
         query = query.view(-1, hidden_states.size(1), self.num_heads, self.head_dim).transpose(1, 2)
         key = key.view(-1, hidden_states.size(1), self.num_heads, self.head_dim).transpose(1, 2)
         value = value.view(-1, hidden_states.size(1), self.num_heads, self.head_dim).transpose(1, 2)
-        length = key.size(1)
-        attn_weights = torch.matmul(query, key.transpose(-1, -2)) / self.head_dim
-        causal_mask = self.bias[1, 1, : length, length]
+        length = key.size(2)
+
+        attn_weights = (query @ key.transpose(-2, -1)) * (1.0 / math.sqrt(key.size(-1)))
+        causal_mask = self.bias[:, :, : length, :length]
         mask_value = torch.finfo(attn_weights.dtype).min
         mask_value = torch.tensor(mask_value, dtype=attn_weights.dtype).to(attn_weights.device)
-        attn_weights = torch.where(causal_mask, attn_weights, mask_value)
+        #attn_weights = torch.where(causal_mask, attn_weights, mask_value)
+        attn_weights = attn_weights.masked_fill(causal_mask==False, mask_value)
         if pad_mask is not None:
             if pad_mask.dim() == 2:
                 pad_mask = pad_mask.unsqueeze(1).unsqueeze(-1)
             pad_mask = pad_mask.to(dtype=self.dtype)
             pad_mask = (1.0 - pad_mask) * torch.finfo(self.dtype).min
-        attn_weights = attn_weights + pad_mask
+            attn_weights = attn_weights + pad_mask
+
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1)
+        attn_weights = self.attn_dropout(attn_weights)
         attn_output = torch.matmul(attn_weights, value)
         attn_output = attn_output.transpose(1, 2).contiguous().view(-1, length, self.embed_dim)
         attn_output = self.c_proj(attn_output)
         attn_output = self.resid_dropout(attn_output)
+
         return attn_output
+
+
+
+
+class MyGPT2Config:
+    def __init__(self, model_name="gpt2"):
+        tmp_config = GPT2Config.from_pretrained(model_name)
+        config_dict = tmp_config.to_dict()
+        for key, value in config_dict.items():
+            setattr(self, key, value)
+        self.hidden_size = 768
+        self.max_position_embedding = 1024
+        self.embedding_dim = 768
+        self.n_layer = 12
+        self.attn_pdrop = 0.1
+
+def get_pretrained_model():
+    tmp = MyGPT2Config()
+    hf = GPT2LMHeadModel.from_pretrained('gpt2')
+    print(hf)
+    model = GPT2(tmp)
+    state = model.state_dict()
+    hf_state = hf.state_dict()
+    print(state.keys())
+    print(hf_state.keys())
+    transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight'] # conv 1d 말고 linear로
+    for k in state.keys():
+        print(k, state[k].shape, hf_state[k].shape)
+        if any(k.endswith(w) for w in transposed):
+            with torch.no_grad():
+                state[k].copy_(hf_state[k].t())
+        else:
+            with torch.no_grad():
+                state[k].copy_(hf_state[k])
+
+
+    model.load_state_dict(state)
+    return model
+
+if __name__ == "__main__":
+    model = get_pretrained_model()
+    model = model.cuda()
+    input_text = "Hello!!!!!!!!!!!!!!!!, my name is"
+    prompt = "Hello!!!!!!!!!!!!!!!!, my name is"
+    model.to('cuda')
+    model.eval()
+
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    encoded_input = tokenizer(prompt, return_tensors='pt').to('cuda')
+
+    x1 = encoded_input['input_ids']
+
+    logits1, loss = model(x1)
+
+    # now draw the argmax samples from each
+    y1 = model.generate(x1, max_new_tokens=100, do_sample=False)[0]
+
+    out1 = tokenizer.decode(y1.cpu().squeeze())
+
+    print(out1)
